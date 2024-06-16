c6ab3e03f9d53d0cceecef90037c67f463ff1a59
wally thioye <wallythioye56@gmail.com>
2024-06-16 03:38:15 +0000

    Mon deuxieme commit

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const app = express();
 
 app.use(express.json());
 
+const allowedOrigins = [
+  'http://localhost:3000', // pour le développement local
+  'https://red-team-product.vercel.app' // pour votre domaine de production
+];
+
 // Middleware pour servir les images statiques
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
@@ -21,10 +26,22 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: 'http://localhost:3001', // Spécifiez l'origine de votre frontend
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Si vous utilisez des cookies
+  origin: function (origin, callback) {
+    // Autorisez les requêtes sans origine (par exemple, les requêtes curl ou postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
+
+// app.use(cors({
+//   origin: 'http://localhost:3001', // Spécifiez l'origine de votre frontend
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true, // Si vous utilisez des cookies
+// }));
 
 app.use('/api/users', userRoutes);
 
